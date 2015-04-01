cabac788f3cfea571ef46ba2a69261f908f451f4
rogersillito <rogersillito@gmail.com>
2015-04-01 15:31:40 +0100

    updated startup script

--- MB_START.bat
+++ MB_START.bat
@@ -3,7 +3,7 @@ SET browser="C:\Program Files (x86)\Google\Chrome\Application" chrome.exe
 EXPLORER "%_MbDir%"
 START python recreate_MBHotKeys_files.py"
 @REM START python "%_MbDir%\_OddsMonkey (SkyBet £10 Q Alert)"
-START /d %browser% "https://smarkets.com/sport/horse-racing/" "http://www.betdaq.com/UI/?siteTab=exchange" "http://sports.betfair.com/horse-racing/"
+START /d %browser% "https://smarkets.com/sport/horse-racing/" "http://www.betdaq.com/UI/?siteTab=exchange" "https://www.betfair.com/exchange/horse-racing" "https://www.matchbook.com/"
 @REM START /d %browser% "https://www.skybet.com/horse-racing" "https://odds.betsafe.com/en/"
 @REM START /d %browser% "http://www.freebets4all.com/automatcher/"
 START /d %browser% "http://www.oddsmonkey.com/PremiumSearch.aspx"
